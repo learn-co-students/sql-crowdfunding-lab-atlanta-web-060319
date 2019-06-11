4dec44e8ae9f56a967c951b7c62defe44fd53688
github username <github email address>
2019-06-11 12:43:15 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,3 +1,5 @@
+
+
 # Write your sql queries in this file in the appropriate method like the example below:
 #
 # def select_category_from_projects
@@ -6,26 +8,52 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
+# **Create a schema based on the following information:**
+# - A project has a title, a category, a funding goal, a start date, and an end date.
+# - A user has a name and an age
+# - A pledge has an amount. It belongs to a user, and it also belongs to a project.
+
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY(projects.title) ;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+
+"SELECT users.name, users.age, SUM(pledges.amount) 
+FROM users INNER JOIN pledges 
+ON users.id = pledges.user_id 
+GROUP BY users.name ORDER BY name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_over_goal
+FROM projects INNER JOIN pledges 
+ON projects.id = pledges.project_id
+GROUP BY projects.title
+HAVING amount_over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) AS amount
+FROM pledges INNER JOIN users 
+ON pledges.user_id = users.id 
+GROUP BY users.name 
+ORDER BY amount ASC; "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount
+FROM projects INNER JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'music';"
 end
 
-def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category 
+"SELECT projects.category, SUM(pledges.amount) 
+FROM projects INNER JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.category
+HAVING projects.category = 'books';"
 end
